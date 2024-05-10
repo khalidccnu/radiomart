e972beaa1a787de5db56b0aa622e1d875f4faa43
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2024-05-10 11:09:33 +0600

    contact form section added

---
+++ src/@modules/contact/ContactFormSection.tsx
@@ -0,0 +1,91 @@
+import { cn } from '@lib/utils';
+import { Button, Col, Form, Input, Row } from 'antd';
+import { ClassValue } from 'clsx';
+import Lottie from 'lottie-react';
+import React, { useEffect, useState } from 'react';
+import { FaPaperPlane } from 'react-icons/fa6';
+
+interface IProps {
+  className?: ClassValue;
+}
+
+const ContactFormSection: React.FC<IProps> = ({ className }) => {
+  const [formInstance] = Form.useForm();
+  const [anmContact, setAnmContact] = useState(null);
+
+  useEffect(() => {
+    import(`@lib/assets/animation/contact.json`).then((response) => setAnmContact(response.default));
+  }, []);
+
+  return (
+    <section className={cn('contact_form_section', className)}>
+      <div className="container">
+        <h2 className="section_title text-center">Drop us a message for any query</h2>
+        <div className="wrapper">
+          <div className="anm_wrapper md:h-[28rem] -mt-12 -mb-24">
+            {anmContact ? <Lottie className="md:h-full" animationData={anmContact} loop={true} /> : null}
+          </div>
+          <div className="form_wrapper">
+            <Form layout="vertical" form={formInstance}>
+              <Row gutter={[{ xs: 0 }, { xs: 16, md: 16 }]}>
+                <Col xs={24}>
+                  <Form.Item
+                    name="name"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Please write your name!',
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Name" />
+                  </Form.Item>
+                </Col>
+                <Col xs={24}>
+                  <Form.Item
+                    name="email"
+                    rules={[
+                      {
+                        type: 'email',
+                        required: true,
+                        message: 'Please write your email!',
+                      },
+                    ]}
+                  >
+                    <Input placeholder="Email address" />
+                  </Form.Item>
+                </Col>
+                <Col xs={24}>
+                  <Form.Item
+                    name="message"
+                    rules={[
+                      {
+                        required: true,
+                        message: 'Please write your message!',
+                      },
+                    ]}
+                  >
+                    <Input.TextArea rows={5} style={{ resize: 'none' }} placeholder="Write something..." />
+                  </Form.Item>
+                </Col>
+                <Col xs={24}>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    size="large"
+                    icon={<FaPaperPlane size={24} />}
+                    className="w-full"
+                  >
+                    Send Message
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactFormSection;
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,12 +2,14 @@ import BaseHeroWrapper from '@base/components/BaseHeroWrapper';
 import PageWrapper from '@base/container/PageWrapper';
 import { paths } from '@lib/constant';
 import ContactDetailSection from '@modules/contact/ContactDetailSection';
+import ContactFormSection from '@modules/contact/ContactFormSection';
 
 const ContactPage = () => {
   return (
     <PageWrapper title="Contact us">
       <BaseHeroWrapper title="Contact" breadcrumb={[{ name: 'contact', slug: paths.contact }]} />
       <ContactDetailSection className="py-14" />
+      <ContactFormSection className="pb-14" />
     </PageWrapper>
   );
 };
